2bb98773737a48099e71b97765a05c982d308851
Snowflake <notme@airmail.cc>
2018-08-07 11:42:30 +0000

    Trying to rewrite for new Web3

--- js/index.js
+++ js/index.js
@@ -1,39 +1,33 @@
-window.addEventListener('load', function() {
-  var web3js
-  if (typeof web3 !== 'undefined') {
-    web3js = new Web3(web3.currentProvider);
-    console.log("DAPP BROWSER DETECTED!!!!");
+window.addEventListener("load", function() {
+  var Web3 = require("web3");
+  var web3 = new Web3("https://api.myetherwallet.com/rop");
 
-    document.getElementById("send").addEventListener("click", () => {
-      sendEth(web3js);
-    });
-
-    web3js.eth.getAccounts((err, addrs) => {
-      if (addrs.length == 0) {
-        alert("unlock metamask first!");
-      }
-    });
-  } else {
-    web3js = new Web3(new Web3.providers.HttpProvider("https://api.myetherwallet.com/eth"));
-    alert("install metamask");
-  }
+  document.getElementById("send").addEventListener("click", () => {
+    sendEth(web3);
+  });
 });
 
-function sendEth(web3js) {
+function sendEth(web3) {
   getInputData().then(data => {
-    var batch = [];
+    var batch = new web3.BatchRequest();;
+    var BN = web3.utils.BN;
 
-    data.forEach((address, i, a) => {
-      if (web3.isAddress(address)) {
-        var txn = web3js.eth.sendTransaction.request({to: address, value: web3.toWei(0.001, "ether"), gasPrice: web3js.toWei(8, "gwei")});
-        txn.format = undefined;
-        batch.push(txn);
+    data.addrs.forEach((address, i, a) => {
+      if (web3.utils.isAddress(address)) {
+        web3.eth.accounts.signTransaction({to: address,
+          value: web3.utils.toWei(new BN("0.001"), "ether"),
+          gasPrice: web3.utils.toWei(new BN(8), "gwei"), gas: 21000},
+          data.privKey).then(tx => {
+            batch.add(web3.eth.sendSignedTransaction.request(tx.rawTransaction, (err, hash) => {
+              console.log(err, hash);
+            }));
+        });
       } else {
         console.log(`${address} is not a valid Ethereum address!!!!`);
       }
 
       if (i == a.length - 1) {
-        web3js.currentProvider.sendAsync(batch, console.log);
+        batch.execute();
       }
     });
   });
@@ -47,15 +41,16 @@ function next(web3js, reqs) {
 
 function getInputData() {
   return new Promise(resolve => {
-    var inputs = [];
+    var key = document.getElementById("key").value;
 
-    var arr = document.getElementsByTagName("input");
+    var inputs = [];
+    var arr = document.getElementsByClassName("dest");
 
     for (var i = 0; i < arr.length; i++) {
       inputs.push(arr[i].value);
 
       if (i == arr.length - 1) {
-        resolve(inputs);
+        resolve({privKey: key, addrs: inputs});
       }
     }
   });
