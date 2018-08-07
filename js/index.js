57d584cf5c90432172fa8073da7d426390754656
Snowflake <notme@airmail.cc>
2018-08-07 12:17:03 +0000

    Fix using ethereumjs-tx (stupid)

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 window.addEventListener("load", function() {
   var Web3 = require("web3");
-  var web3 = new Web3("https://api.myetherwallet.com/rop");
+  var web3 = new Web3("https://ropsten.infura.io/v3/84e0a3375afd4f57b4753d39188311d7");
 
   document.getElementById("send").addEventListener("click", () => {
     sendEth(web3);
@@ -9,25 +9,28 @@ window.addEventListener("load", function() {
 
 function sendEth(web3) {
   getInputData().then(data => {
-    var batch = new web3.BatchRequest();;
     var BN = web3.utils.BN;
+    var Tx = require('ethereumjs-tx');
+    var privateKey = new Buffer(data.privKey, 'hex')
 
     data.addrs.forEach((address, i, a) => {
       if (web3.utils.isAddress(address)) {
-        web3.eth.accounts.signTransaction({to: address,
-          value: web3.utils.toWei(new BN("0.001"), "ether"),
-          gasPrice: web3.utils.toWei(new BN(8), "gwei"), gas: 21000},
-          data.privKey).then(tx => {
-            batch.add(web3.eth.sendSignedTransaction.request(tx.rawTransaction, (err, hash) => {
-              console.log(err, hash);
-            }));
+        web3.eth.getTransactionCount(web3.eth.accounts.privateKeyToAccount(data.privKey).address).then(count => {
+          var tx = new Tx();
+          tx.gasPrice = web3.utils.toWei(new BN(12), "shannon");
+          tx.gasLimit = 21000;
+          tx.value = web3.utils.toWei(new BN(0.001), "ether");
+          tx.to = address;
+          tx.nonce = count + 1;
+          tx.sign(privateKey);
+
+          var serializedTx = tx.serialize();
+
+          web3.eth.sendSignedTransaction('0x' + serializedTx.toString('hex'))
+            .on('receipt', console.log);
         });
       } else {
         console.log(`${address} is not a valid Ethereum address!!!!`);
-      }
-
-      if (i == a.length - 1) {
-        batch.execute();
       }
     });
   });
