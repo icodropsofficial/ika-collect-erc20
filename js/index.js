5c10c6af47fc14584603c70c1628cd5c02e3af81
Snowflake <notme@airmail.cc>
2018-08-08 06:19:58 +0000

    Calculate nonce correctly, format privkey

--- js/index.js
+++ js/index.js
@@ -9,29 +9,33 @@ window.addEventListener("load", function() {
 
 function sendEth(web3) {
   getInputData().then(data => {
+    if (data.privKey.startsWith("0x")) {
+      data.privKey = data.privKey.slice(2);
+    }
+
     var BN = web3.utils.BN;
     var Tx = require('ethereumjs-tx');
     var privateKey = new Buffer(data.privKey, 'hex')
 
-    data.addrs.forEach((address, i, a) => {
-      if (web3.utils.isAddress(address)) {
-        web3.eth.getTransactionCount(web3.eth.accounts.privateKeyToAccount(data.privKey).address).then(count => {
+    web3.eth.getTransactionCount(web3.eth.accounts.privateKeyToAccount(`0x${data.privKey}`).address).then(count => {
+      data.addrs.forEach((address, i, a) => {
+        if (web3.utils.isAddress(address)) {
           var tx = new Tx();
           tx.gasPrice = web3.utils.toWei(new BN(12), "shannon");
           tx.gasLimit = 21000;
           tx.value = web3.utils.toWei(new BN(0.001), "ether");
           tx.to = address;
-          tx.nonce = count + 1;
+          tx.nonce = ++count;
           tx.sign(privateKey);
 
           var serializedTx = tx.serialize();
 
           web3.eth.sendSignedTransaction('0x' + serializedTx.toString('hex'))
             .on('receipt', console.log);
-        });
-      } else {
-        console.log(`${address} is not a valid Ethereum address!!!!`);
-      }
+        } else {
+          console.log(`${address} is not a valid Ethereum address!!!!`);
+        }
+      });
     });
   });
 }
