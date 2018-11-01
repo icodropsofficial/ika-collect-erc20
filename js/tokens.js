59e42150247e8539405838fe3f08bbd629a1978f
Snowflake <notme@airmail.cc>
2018-11-01 09:33:35 +0000

    Add LICENSE

--- js/tokens.js
+++ js/tokens.js
@@ -1,3 +1,21 @@
+/**
+ * IKA — a tool that makes it easier to distribute/collect ETH/ERC20 tokens.
+ * Copyright (C) 2018  ICODROPS
+ *
+ * IKA is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * IKA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 'use strict';
 
 const abi = [
